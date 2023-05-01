a728f9a8dc4bd54b80235aeae96c9078c91fbc05
Hakovo66 <113375465+Hakovo66@users.noreply.github.com>
2023-04-30 23:10:16 -0600

    Create Ejercicio-DoWhile.java

---
+++ Ejercicio-DoWhile.java
@@ -0,0 +1,15 @@
+public class EjercicioDoWhile {
+
+    public static void main(String[] args) {
+
+        int numeroDoWhile = 1;
+
+        do {
+            System.out.println(numeroDoWhile);
+            numeroDoWhile = numeroDoWhile + 1;
+        } while (numeroDoWhile <= 3);
+               
+
+    }
+    
+}
